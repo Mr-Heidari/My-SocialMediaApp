d8188872afe00c248738b97154cca2eaa2fcc2e3
AmirrezaHeidari <amirheidari020@gmail.com>
2024-05-07 02:15:22 +0430

    top bar implementation ended . topbar only show on mobile size

--- src/components/ui/shared/Topbar.tsx
+++ src/components/ui/shared/Topbar.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "../button";
+import { useSignOutAccount } from "@/lib/reat-query/queriesAndMutation";
+import { useUserContext } from "@/context/AuthContext";
 
 const Topbar = () => {
-  return (
-    <div>Topbar</div>
-  )
-}
+  //sign out our user and delete user session from dratabase 
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
 
-export default Topbar
+  const navigate = useNavigate();
+  
+  //we can access user information with useContext
+  const { user } = useUserContext();
+
+  //check if sign out success
+  useEffect(() => {
+    if (isSuccess) navigate(0);
+  }, [isSuccess]);
+
+  return (
+    <section className="topbar">
+      <div className=" flex-between py-2 px-5 ">
+        <Link to="/" className="flex gap-33 items-center">
+          <img
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={130}
+            height={325}
+          />
+        </Link>
+
+        <div className="gap-4 flex">
+          <Button
+            variant="ghost"
+            className="shad-button_ghost"
+            onClick={() => signOut()}
+          >
+            <img src="/assets/icons/logout.svg" alt="logout" />
+          </Button>
+          <Link to={`/profile/${user.id}`} className=" flex-center gap-3">
+            <img
+              src={user.imageUrl || "/assets/images/profile-placeholder.svg"}
+              alt="profile"
+              className="h-8 w-8 rounded-full"
+            />
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Topbar;
