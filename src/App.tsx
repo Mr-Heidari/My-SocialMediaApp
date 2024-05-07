b2c3abddc3c414a884e74a939296cc4a3b400031
AmirrezaHeidari <amirheidari020@gmail.com>
2024-05-07 05:01:42 +0430

    all routes need added

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,21 @@ import { Route, Routes } from "react-router-dom";
 import "./globals.css";
 
 import SigninForm from "./_auth/forms/SigninForm";
-import { Home } from "./_root/pages";
+import {
+  AllUsers,
+  CreatePost,
+  EditPost,
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from "./_root/pages";
 import SingupForm from "./_auth/forms/SingupForm";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
 
 export const App = () => {
   return (
@@ -22,9 +32,17 @@ export const App = () => {
         {/**private routes : only ppl are sing in can see */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-pos:id" element={<EditPost />} />
+          <Route path="/posts:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
-      <Toaster/>
+      <Toaster />
     </main>
   );
 };
