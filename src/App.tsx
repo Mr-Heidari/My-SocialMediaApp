7955d9f06c9816f7b740538b6ea0d956d079a981
AmirrezaHeidari <amirheidari020@gmail.com>
2024-05-18 12:12:24 +0430

    change toaster place and some padding fixed

--- src/App.tsx
+++ src/App.tsx
@@ -22,30 +22,32 @@ import PersonalizationForm from "./_auth/forms/PersonalizationForm";
 
 export const App = () => {
   return (
-    <main className=" flex h-screen">
-      <Routes>
-        <Route path="/pesonalizeProfile" element={<PersonalizationForm />} />
-        <Route element={<AuthLayout />}>
-          {/**public routes : every body can see */}
-          <Route path="/sign-in" element={<SigninForm />} />
-          <Route path="/sign-up" element={<SingupForm />} />
-        </Route>
-        {/** only new User can see this page */}
+    <>
+      <main className=" flex h-screen">
+        <Routes>
+          <Route path="/pesonalizeProfile" element={<PersonalizationForm />} />
+          <Route element={<AuthLayout />}>
+            {/**public routes : every body can see */}
+            <Route path="/sign-in" element={<SigninForm />} />
+            <Route path="/sign-up" element={<SingupForm />} />
+          </Route>
+          {/** only new User can see this page */}
 
-        {/**private routes : only ppl are sing in can see */}
-        <Route element={<RootLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/explore" element={<Explore />} />
-          <Route path="/saved" element={<Saved />} />
-          <Route path="/all-users" element={<AllUsers />} />
-          <Route path="/create-post" element={<CreatePost />} />
-          <Route path="/update-post/:id" element={<EditPost />} />
-          <Route path="/posts/:id" element={<PostDetails />} />
-          <Route path="/profile/:id/*" element={<Profile />} />
-          <Route path="/update-profile/:id" element={<UpdateProfile />} />
-        </Route>
-      </Routes>
+          {/**private routes : only ppl are sing in can see */}
+          <Route element={<RootLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/saved" element={<Saved />} />
+            <Route path="/all-users" element={<AllUsers />} />
+            <Route path="/create-post" element={<CreatePost />} />
+            <Route path="/update-post/:id" element={<EditPost />} />
+            <Route path="/posts/:id" element={<PostDetails />} />
+            <Route path="/profile/:id/*" element={<Profile />} />
+            <Route path="/update-profile/:id" element={<UpdateProfile />} />
+          </Route>
+        </Routes>
+      </main>
       <Toaster />
-    </main>
+    </>
   );
 };
--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,18 +1,21 @@
 import Loader from "@/components/ui/shared/Loader";
 import PostCard from "@/components/ui/shared/PostCard";
+import { useToast } from "@/components/ui/use-toast";
 import { useGetRecentPosts} from "@/lib/reat-query/queriesAndMutation";
 import { Models } from "appwrite";
 
 
 const Home = () => {
-  
+  const { toast } = useToast();
   const {data:posts , isPending:isPostLoading }=useGetRecentPosts()
-
+  function tokhmi(){
+   return  toast({ title: "title: Sign in failed. Please try again." });
+  }
   return (
     <div className="flex flex-1  ">
       <div className="home-container">
         <div className="home-posts">
-          <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+          <h2 className="h3-bold md:h2-bold text-left w-full"onClick={tokhmi}>Home Feed</h2>
           {isPostLoading && !posts ? (<Loader width={30} height={30}/>):(
             <ul className="flex flex-col flex-1 gap-9 w-full">
               {posts?.documents.map((post :Models.Document)=>(
